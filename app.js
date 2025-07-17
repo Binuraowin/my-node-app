5f25ea451550942f33964e2a3a3c74a501f4945a
binuraowin <binura.owin1@gmail.com>
2025-07-18 00:37:31 +0530

    Update message

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello from Node.js on EC2!'));
+app.get('/', (req, res) => res.send('Hello, CI/CD!'));
 app.listen(8000, () => console.log('Server running on port 8000'));
